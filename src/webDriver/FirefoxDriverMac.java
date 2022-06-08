394483d44c370b8b0340e7758fd8ecbe1c0d9e88
Nishant Ranjan <nishantranjan@Nishants-MacBook-Pro-2.local>
2022-06-08 18:36:23 +0530

    find elements

--- src/webDriver/FirefoxDriverMac.java
+++ src/webDriver/FirefoxDriverMac.java
@@ -8,7 +8,10 @@ public class FirefoxDriverMac {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-//		System.setProperty("webdriver.gecko.driver", "/Users/nishantranjan/projects/selenium-revison/drivers/geckodriver");
+		//		System.setProperty("webdriver.gecko.driver", "/Users/nishantranjan/projects/selenium-revison/drivers/geckodriver");
+
+		//system property has been set so, we don't need to define property every time.
+		
 		WebDriver driver = new FirefoxDriver();
 
 		String baseURL = "https://www.google.com";
